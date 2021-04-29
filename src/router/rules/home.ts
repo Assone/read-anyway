93504803f8775ac87f237dd746f2e8bf8b17e48f
a3onny <a3onny@gmail.com>
2021-04-29 18:53:51 +0800

    feat(view): 添加注册页

--- src/router/rules/home.ts
+++ src/router/rules/home.ts
@@ -13,6 +13,11 @@ const rules: RouteConfig[] = [
     name: 'Login',
     component: () => import(/* webpackChunkName: "login" */ '@/views/Login.vue'),
   },
+  {
+    path: '/register',
+    name: 'Register',
+    component: () => import(/* webpackChunkName: "register" */ '@/views/Register.vue'),
+  },
 ];
 
 export default rules;
---
+++ src/views/Register.vue
@@ -0,0 +1,38 @@
+<template lang="pug">
+.view-register
+  el-form(:model='form', label-position='right', label-width='80px')
+    el-form-item
+      h1 注册
+    el-form-item(label='邮箱')
+      el-input(v-model='form.email', placeholder='请输入邮箱', type='email')
+    el-form-item(label='昵称')
+      el-input(v-model='form.nickname', placeholder='请输入昵称')
+    el-form-item(label='密码')
+      el-input(v-model='form.password', placeholder='请输入密码', type='password', show-password)
+    el-form-item(label='确认密码')
+      el-input(v-model='form.confirm', placeholder='请输入确认密码', type='password', show-password)
+    el-form-item(label='验证码')
+      el-input(v-model='form.verification', placeholder='请输入验证码')
+    el-form-item
+      el-checkbox(v-model='form.agree') 我同意xxxxx
+    el-form-item
+      el-button 注册
+    el-form-item
+      p 已有账号，请点击这里#[router-link(to='/login') 登陆]
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Register extends Vue {
+  form = {
+    email: '',
+    nickname: '',
+    password: '',
+    confirm: '',
+    verification: '',
+    agree: false,
+  };
+}
+</script>
